3d4298e882fecb0b67569044f3cf93670a17ed6c
Tommy Cervenka <tommy@cervenka.org>
2020-11-15 20:23:45 -0600

    fixed my css file organization so my route would work and got my test page working with the alert hello world

---
+++ src/js/index.js
@@ -0,0 +1,11 @@
+//importing main bootstrap style sheet
+import "bootstrap/dist/css/bootstrap.min.css";
+//importing our custom style sheet
+import "../style.css";
+
+//import jquery
+import $ from "jquery";
+
+$(() => {
+    alert('Hello world')
+});
